8720a8fd3caec58266f5edb49f43cec889500d8b
txtart <kane7274@kakao.com>
2021-10-15 22:23:35 +0900

    feat: 메뉴 영역 개발
    메뉴 아이템 컴포넌트 작성
    메뉴 영역 개발
    flex 적용
    ul reset css 적용

---
+++ src/components/MenuItem.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+const Title = styled.div`
+    font-size: 20px;
+`
+
+const Price = styled.div`
+    font-size: 20px;
+`
+
+// thumbnail
+const FoodImg = styled.div`
+    width:130px;
+    height: 130px;
+    background-size:     130px 130px;                     
+    background-repeat:   no-repeat;
+    background-position: center center;  
+    background-image: url("${props => props.src}");
+    padding: 10px 15px 10px 0;
+    background-origin: content-box;
+`;
+
+const ItemWrap = styled.li`
+    width:100%;
+    height: 150px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Infomation = styled.div`  
+    display:flex;
+    flex-direction: column;
+    flex-grow: 1;
+    justify-content: center;
+    padding-left: 20px;
+`;
+
+function MenuItem(props) {
+    return (
+        <ItemWrap>
+            <Infomation>
+                <Title>{props.name}</Title>
+                <Price>{props.price}</Price>
+            </Infomation>
+            <FoodImg src={props.imgSrc} />
+        </ItemWrap>
+    )
+}
+
+export default MenuItem
