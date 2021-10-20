1379d42696a4e1217fd4c3e8065bbfe4dd960318
qorlgns1 <rlgns0610@gmail.com>
2021-10-21 01:19:45 +0900

    feat: 메뉴 리스트 영역 개발
    1. 메뉴 이미지 임시 추가
    2. 리스트 태그 reset css 적용
    3. 메뉴 리스트 영역에 대한 위치를 잡았습니다.
    이벤트 바인드는 안했습니다.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,41 @@ import "./App.css";
 import StoreImage from "./components/StoreImage";
 import SubNavi from "./components/SubNavi";
 import NaviButton from "./components/NaviButton";
+import MenuList from "./components/MenuList";
+import MenuItem from "./components/MenuItem";
+
+const menus = [
+  {
+    name: "항정살 수육",
+    price: "21,000원",
+    imgSrc: "/image/menu/항정살수육.png",
+  },
+  {
+    name: "돼지고기 가지볶음",
+    price: "18,000원",
+    imgSrc: "/image/menu/돼지고기가지볶음.png",
+  },
+  {
+    name: "모듬 치킨",
+    price: "17,000원",
+    imgSrc: "/image/menu/모듬치킨.png",
+  },
+  {
+    name: "항정살 수육",
+    price: "21,000원",
+    imgSrc: "/image/menu/항정살수육.png",
+  },
+  {
+    name: "돼지고기 가지볶음",
+    price: "18,000원",
+    imgSrc: "/image/menu/돼지고기가지볶음.png",
+  },
+  {
+    name: "모듬 치킨",
+    price: "17,000원",
+    imgSrc: "/image/menu/모듬치킨.png",
+  },
+];
 
 function App() {
   return (
@@ -11,6 +46,13 @@ function App() {
         <NaviButton>메인 메뉴</NaviButton>
         <NaviButton>주류</NaviButton>
       </SubNavi>
+      <MenuList>
+        {menus.map(({ name, price, imgSrc }, index) => {
+          return (
+            <MenuItem key={index} name={name} price={price} imgSrc={imgSrc} />
+          );
+        })}
+      </MenuList>
     </>
   );
 }
