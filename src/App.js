372b49a25d32888aa5ad7bc87b4e502f58952565
qorlgns1 <rlgns0610@gmail.com>
2021-10-21 00:57:54 +0900

    feat: 메뉴 영역개발
    메뉴 네비게이션 영역 개발
    메뉴 네비게이션 버튼 개발
    button reset css 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import StoreImage from "./components/StoreImage";
+import SubNavi from "./components/SubNavi";
+import NaviButton from "./components/NaviButton";
 
 function App() {
   return (
     <>
       <StoreImage />
+      <SubNavi>
+        <NaviButton>메인 메뉴</NaviButton>
+        <NaviButton>주류</NaviButton>
+      </SubNavi>
     </>
   );
 }
---
+++ src/components/SubNavi.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  height: 45px;
+  border-bottom: solid 1px #e0e0e0;
+`;
