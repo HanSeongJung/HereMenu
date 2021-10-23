34264708504fb8069a98a58a7564719fa4335922
qorlgns1 <rlgns0610@gmail.com>
2021-10-23 17:20:53 +0900

    feat: 메뉴 상수값 추가
    메인메뉴, 주류 상수값 추가

---
+++ src/constant/MENUS.js
@@ -0,0 +1,68 @@
+const MENUS = {
+  mainMenu: [
+    {
+      name: "항정살 수육",
+      price: "21,000원",
+      imgSrc: "/image/menu/항정살수육.png",
+    },
+    {
+      name: "돼지고기 가지볶음",
+      price: "18,000원",
+      imgSrc: "/image/menu/돼지고기가지볶음.png",
+    },
+    {
+      name: "모듬 치킨",
+      price: "17,000원",
+      imgSrc: "/image/menu/모듬치킨.png",
+    },
+    {
+      name: "항정살 수육",
+      price: "21,000원",
+      imgSrc: "/image/menu/항정살수육.png",
+    },
+    {
+      name: "돼지고기 가지볶음",
+      price: "18,000원",
+      imgSrc: "/image/menu/돼지고기가지볶음.png",
+    },
+    {
+      name: "모듬 치킨",
+      price: "17,000원",
+      imgSrc: "/image/menu/모듬치킨.png",
+    },
+  ],
+  alcohol: [
+    {
+      name: "감자술",
+      price: "7,000원",
+      imgSrc: "/image/alcohol/감자술.png",
+    },
+    {
+      name: "매실원주",
+      price: "8,000원",
+      imgSrc: "/image/alcohol/매실원주.png",
+    },
+    {
+      name: "빙탄복",
+      price: "10,000원",
+      imgSrc: "/image/alcohol/빙탄복.png",
+    },
+    {
+      name: "솔송주",
+      price: "12,000원",
+      imgSrc: "/image/alcohol/솔송주.png",
+    },
+    {
+      name: "왕주",
+      price: "12,000원",
+      imgSrc: "/image/alcohol/왕주.png",
+    },
+    {
+      name: "오메기술",
+      price: "12,000원",
+      imgSrc: "/image/alcohol/오메기술.png",
+    },
+  ],
+};
+
+export default MENUS;
